7550b743e2298613fdb475cc26bf509574c04a32
mvcjhkim <mvcjhkim@gmail.com>
2022-05-30 01:03:57 +0900

    DOM을 이용하여 HTML 객체 저장.

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const userId = document.querySelector("#userId");
+const passwd= document.querySelector("#passwd");
+const button = document.querySelector("#submit");
+
+button.addEventListener("click",login);
+
+function login(){
+    const req = {
+        userId : userId.value,
+        passwd : passwd.value
+    }
+    console.log(req);
+}
